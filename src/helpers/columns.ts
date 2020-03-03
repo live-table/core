cb138a2125fdaa31e12ff7341400beaf5544dda4
Dorian Herlory <dorian.herlory@gmail.com>
2020-03-03 23:07:58 +0100

    Add a type checking function for child columns.

--- src/helpers/columns.ts
+++ src/helpers/columns.ts
@@ -9,3 +9,13 @@ export function isParent<DataRow>(
 ): column is Core.ParentColumn<DataRow> {
 	return undefined !== (column as Core.ParentColumn<DataRow>).columns
 }
+
+/**
+ * Check whether the specified column is a child one.
+ * @param column
+ */
+export function isChild<DataRow>(
+	column: Core.Column<DataRow>
+): column is Core.ChildColumn<DataRow> {
+	return !isParent(column)
+}
