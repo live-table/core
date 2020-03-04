8aa733a5432b7125fc8a22b45ec0d4d898bb3efc
Dorian Herlory <dorian.herlory@gmail.com>
2020-03-04 23:41:57 +0100

    Add unit tests for columns helpers.

---
+++ src/helpers/columns.test.ts
@@ -0,0 +1,36 @@
+import * as Core from ".."
+import { isParent, isChild } from "./columns"
+
+const childColumn: Core.Column<Foo> = {
+	data: "id",
+	label: "Child column"
+}
+
+const parentColumn: Core.Column<Foo> = {
+	columns: [],
+	label: "Parent column"
+}
+
+describe("isParent()", () => {
+	it("should be false", () => {
+		expect(isParent(childColumn)).toBe(false)
+	})
+
+	it("should be true", () => {
+		expect(isParent(parentColumn)).toBe(true)
+	})
+})
+
+describe("isChild()", () => {
+	it("should be false", () => {
+		expect(isChild(parentColumn)).toBe(false)
+	})
+
+	it("should be true", () => {
+		expect(isChild(childColumn)).toBe(true)
+	})
+})
+
+interface Foo {
+	id: number;
+}
