eac2f545e42dfe367eae9ddfdf1522f9172002a7
Dorian Herlory <dorian.herlory@gmail.com>
2020-03-04 21:58:59 +0100

    Add some helper methods.

---
+++ src/helpers/live-table.ts
@@ -0,0 +1,44 @@
+import * as Core from ".."
+
+/**
+ * Check whether the {@link "index".LiveTable} binded to the `this` parameter is
+ * searchable.
+ *
+ * This helper is provided as a base implementation of
+ * {@link "index".LiveTable#isSearchable}
+ *
+ * @param this The live-table implementation.
+ */
+export function isSearchable<DataRow>(this: Core.LiveTable<DataRow>): boolean {
+	return undefined !== this.search
+}
+
+/**
+ * Check whether the {@link "index".LiveTable} binded to the `this` parameter
+ * has at least one {@link "index".Action}.
+ *
+ * @param this The live-table implementation.
+ */
+export function hasActions<DataRow>(this: Core.LiveTable<DataRow>): boolean {
+	return !!this.actions && this.actions.length > 0
+}
+
+/**
+ * Check whether the {@link "index".LiveTable} binded to the `this` parameter
+ * should display its header.
+ *
+ * A {@link "index".LiveTable} should display its header if at least one of the
+ * following conditions is `true`:
+ * * The live-table is searchable.
+ * * The live-table has at least one action.
+ *
+ * @param this The live-table implementation.
+ *
+ * @see {@link isSearchable}
+ * @see {@link hasActions}
+ */
+export function shouldDisplayHeader<DataRow>(
+	this: Core.LiveTable<DataRow>
+): boolean {
+	return this.isSearchable()
+}
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,95 @@
 import * as ClassNames from "./class-names"
 import * as ColumnsHelpers from "./helpers/columns"
+import * as LiveTableHelpers from "./helpers/live-table"
 
 export {
 	ClassNames,
-	ColumnsHelpers
+	ColumnsHelpers,
+	LiveTableHelpers
+}
+
+/**
+ * The specification of an action generaly represented by a button.
+ */
+export interface Action {
+	/** The text to display in the button. */
+	label: string;
+	/**
+	 * The text to display as a tooltip on mouse over.
+	 *
+	 * If undefined, the live-table should display the label as tooltip.
+	 */
+	title?: string;
+	/**
+	 * The callback function executed when the user clicks the button.
+	 */
+	onClick(): void;
 }
 
 export interface BaseColumn {
 	/** The label to display. */
 	label: string;
 	/**
-	 * The text to display as a tooltip on mouse over.<br>
+	 * The text to display as a tooltip on mouse over.
+	 *
 	 * If undefined, the live-table should display the label as tooltip.
 	 */
 	title?: string;
 }
 
-export interface ChildColumn<TDataRow> extends BaseColumn {
-	data: ColumnKey<TDataRow>;
+export interface ChildColumn<DataRow> extends BaseColumn {
+	data: ColumnKey<DataRow>;
 }
 
-export interface ColumnCreator<TDataRow> {
-	(colIndex: number): Column<TDataRow>
+export interface ColumnCreator<DataRow> {
+	(colIndex: number): Column<DataRow>
 }
 
-export interface ProceduralColumns<TDataRow> {
+export interface ProceduralColumns<DataRow> {
 	count: number;
-	createColumn: ColumnCreator<TDataRow>;
+	createColumn: ColumnCreator<DataRow>;
 }
 
 /**
  * The base specification of a live-table.
  * @typeParam TDataRow The type of a data row.
  */
-export interface LiveTable<TDataRow> {
-	columns?: ColumnsOption<TDataRow>;
+export interface LiveTable<DataRow> {
+	actions?: Action[];
+	/** The columns definition. */
+	columns?: ColumnsOption<DataRow>;
 	/** The data to display in the live-table. */
-	data: TDataRow[];
+	data: DataRow[];
+	/**
+	 * Check whether the implementation has actions.
+	 *
+	 * This function can return the result of the helper
+	 * {@link "helpers/live-table".hasActions} binding the `this` parameter to the
+	 * implementation of {@link LiveTable}.
+	 */
+	hasActions(): boolean;
+	/**
+	 * Check whether the implementation is searchable.
+	 *
+	 * This function can return the result of the helper
+	 * {@link "helpers/live-table".isSearchable} binding the `this` parameter to the
+	 * implementation of {@link LiveTable}.
+	 */
+	isSearchable(): boolean;
 	/** Whether the live-table has to display the pagination. */
 	pagination?: PaginationOption;
 	/** Whether the live-table has to display the search area. */
-	search?: SearchOption<TDataRow>;
+	search?: SearchOption<DataRow>;
+	/**
+	 * Check whether the implementation should display its header.
+	 *
+	 * This function can return the result of the helper
+	 * {@link "helpers/live-table".shouldDisplayHeader} binding the `this`
+	 * parameter to the implementation of {@link LiveTable}.
+	 */
+	shouldDisplayHeader(): boolean;
 	/** The real table of the live-table. */
-	table: Table<TDataRow> | null;
+	table: Table<DataRow> | null;
 }
 
 /**
@@ -64,37 +111,37 @@ export interface PaginationOption {
 /**
  * The specification of a column nesting columns.
  */
-export interface ParentColumn<TDataRow> extends BaseColumn {
-	columns: Column<TDataRow>[];
+export interface ParentColumn<DataRow> extends BaseColumn {
+	columns: Column<DataRow>[];
 }
 
 /**
  * The specification of a search option.
  * @typeParam TDataRow The type of a data row.
  */
-export interface SearchOption<TDataRow> {
+export interface SearchOption<DataRow> {
 	/**
 	 * @param input The search input.
 	 * @param data The data displayed in the live-table.
 	 * @returns The filtered data matching the
 	 */
-	(input: string, data: TDataRow[]): TDataRow[];
+	(input: string, data: DataRow[]): DataRow[];
 }
 
 /**
  * The specification of a table.
  * @typeParam TDataRow The type of a data row.
  */
-export interface Table<TDataRow> {
+export interface Table<DataRow> {
 	/** The last descendant columns. */
-	columns: ChildColumn<TDataRow>[];
+	columns: ChildColumn<DataRow>[];
 	/** The implementation of {@link Core<TDataRow>} nesting this table. */
-	liveTable: LiveTable<TDataRow>;
+	liveTable: LiveTable<DataRow>;
 }
 
-export type Column<TDataRow> = ChildColumn<TDataRow> | ParentColumn<TDataRow>
-export type ColumnKey<TDataRow> =
-TDataRow extends unknown[] ? number :
-TDataRow extends object ? keyof TDataRow :
+export type Column<DataRow> = ChildColumn<DataRow> | ParentColumn<DataRow>
+export type ColumnKey<DataRow> =
+DataRow extends unknown[] ? number :
+DataRow extends object ? keyof DataRow :
 undefined
-export type ColumnsOption<TDataRow> = Column<TDataRow>[] | ProceduralColumns<TDataRow>
+export type ColumnsOption<DataRow> = Column<DataRow>[] | ProceduralColumns<DataRow>
